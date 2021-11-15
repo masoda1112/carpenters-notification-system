135d1eced90637d0fdb2bf8aa188c9748cc2a675
masoda1112 <wendy.odakura@gmail.com>
2021-11-15 18:08:47 +0900

    lineapiwebhookデバッグ

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -32,7 +32,7 @@ class LineMessengerController extends Controller
 
             // ユーザーにメッセージを返す
             $reply=$bot->replyText($reply_token, $reply_message);
-            Log::debug('$findData="' .$inputs. '"');
+            // Log::debug('$findData="' .$inputs. '"');
 
             return 'ok';
         }
