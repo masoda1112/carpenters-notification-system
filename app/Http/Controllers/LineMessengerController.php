3daf9999b6a229d1428afddc26c02bee57ab35b0
masoda1112 <wendy.odakura@gmail.com>
2021-11-16 15:45:35 +0900

    lineapiwebhooデバッグテスト追加

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Client;
 use Illuminate\Http\Request;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot;
@@ -16,10 +17,15 @@ class LineMessengerController extends Controller
         $inputs=$request->all();
 
         // そこからtypeをとりだし、$message_typeに代入
-        $message_type=$inputs['events'][0]['type'];
+        $hook_type=$inputs['events'][0]['type'];
+
+        $client=new Client();
+        $client->line_id="webhook";
+        $client->name="webhook";
+        $client->save();
 
         // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
-        if($message_type=='message') {
+        if($hook_type=='message') {
             // replyTokenを取得
             $reply_token=$inputs['events'][0]['replyToken'];
 
@@ -28,13 +34,25 @@ class LineMessengerController extends Controller
             $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
 
             // 送信するメッセージの設定
-            $reply_message='メッセージありがとうございます！これはサーバーから送られたメッセージです';
+            $reply_message='ご返信ありがとうございます。登録が完了しました！これはサーバーから送られたメッセージです';
 
             // ユーザーにメッセージを返す
             $reply=$bot->replyText($reply_token, $reply_message);
             // Log::debug('$findData="' .$inputs. '"');
 
+            // LINEのユーザーIDをuserIdに代入
+            $clientId=$request['events'][0]['source']['userId'];
+            // userIdがあるユーザーを検索
+            $client=Client::where('line_id', $userId)->first();
+            // もし見つからない場合は、データベースに保存
+            if($client==NULL) {
+                $client=new Client();
+                $client->line_id=$clientId;
+                $client->name=$request['events'][0]['message']['text'];
+                $client->save();
+            }
             return 'ok';
         }
+        
     }
 }
