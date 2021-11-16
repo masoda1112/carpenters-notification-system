75073f9280bf7fa148f51baad01cc0ad4fbf43ef
masoda1112 <wendy.odakura@gmail.com>
2021-11-16 19:39:57 +0900

    メッセージ応答機能実装中

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -23,7 +23,6 @@ class LineMessengerController extends Controller
         // LINEBOTSDKの設定
         $http_client = new CurlHTTPClient(config('services.line.channel_token'));
         $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
-
         $reply_message=new TextMessageBuilder('ご返信ありがとうございます。');
 
         // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
@@ -38,19 +37,18 @@ class LineMessengerController extends Controller
                 $client->line_id=$clientId;
                 $client->name=$inputs['events'][0]['message']['text'];
                 $client->save();
-
                 // 送信するメッセージの設定
                 $reply_message=new TextMessageBuilder('ご返信ありがとうございます。登録が完了しました！');
             }else{
                 // 送信するメッセージの設定
-                $reply_message=new TextMessageBuilder('ご返信ありがとうございます。すでに登録されております。');
+                $reply_message=new TextMessageBuilder('ご返信ありがとうございます。すでにご登録いただいております');
             }
-            return 'ok';
         }else{
             // 送信するメッセージの設定
             $reply_message=new TextMessageBuilder('ご返信ありがとうございます。申し訳ございませんが、文章を用いてお名前をご返信ください');
         }
         $reply=$bot->replyMessage($reply_token, $reply_message);
+        return 'ok';
     }
 
     public function message() {
