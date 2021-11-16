2a1250da0ca0d042d131bea4947f8080a1ef65ec
masoda1112 <wendy.odakura@gmail.com>
2021-11-16 17:36:12 +0900

    lineapiwebhookテスト修正

--- app/Http/Controllers/CarpenterController.php
+++ app/Http/Controllers/CarpenterController.php
@@ -11,7 +11,7 @@ class CarpenterController extends Controller
     //
     public function index(){
         $carpenters = Carpenter::all();
-        $lineenv = config('services.mailgun.domain');
+        $lineenv = env('LINE_CHANNEL_TOKEN');
         $messengerenv = config('services.line.messenger_secret');
         return view('carpenters')->with(['carpenters' => $carpenters,'lineenv' => $lineenv, 'messengerenv' => $messengerenv]);
     }
