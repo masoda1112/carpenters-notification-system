69efbff13a68009bb430253496dfa0076e2b452b
masoda1112 <wendy.odakura@gmail.com>
2021-11-16 19:24:31 +0900

    メッセージ応答機能実装中

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -15,14 +15,9 @@ class LineMessengerController extends Controller
     public function webhook(Request $request) {
         // LINEから送られた内容を$inputsに代入
         $inputs=$request->all();
-
-        $reply_token = $inputs['events'][0]['replyToken'];
         // そこからtypeをとりだし、$message_typeに代入
         $hook_type=$inputs['events'][0]['type'];
 
-        // LINEBOTSDKの設定
-        $http_client = new CurlHTTPClient(config('services.line.channel_token'));
-        $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
 
         // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
         if($hook_type=='message') {
@@ -32,16 +27,17 @@ class LineMessengerController extends Controller
             // userIdがあるユーザーを検索
             // もし見つからない場合は、データベースに保存
             if($client==NULL) {
+                // 送信するメッセージの設定
+                // LINEBOTSDKの設定
+                $reply_token=$inputs['events'][0]['replyToken'];
+                $http_client=new CurlHTTPClient(config('services.line.channel_token'));
+                $bot=new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
+                $reply_message=new TextMessageBuilder('ご返信ありがとうございます。登録が完了しました！');
+                $reply=$bot->replyText($reply_token, $reply_message);
                 $client=new Client();
-                // $client->line_id=$inputs['events'][0]['source']['userId'];
-                // $client->name=$inputs['events'][0]['message']['text'];
                 $client->line_id=$clientId;
                 $client->name=$inputs['events'][0]['message']['text'];
                 $client->save();
-
-                // 送信するメッセージの設定
-                $reply_message=new TextMessageBuilder('ご返信ありがとうございます。登録が完了しました！');
-                $reply=$bot->replyText($reply_token, $reply_message);
             }else{
                 // 送信するメッセージの設定
                 $reply_message=new TextMessageBuilder('ご返信ありがとうございます。すでに登録されております。');
