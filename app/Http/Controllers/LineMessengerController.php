dcb28a553a5e74d5eeb4b3664bfbbf4c3669d8f5
masoda1112 <wendy.odakura@gmail.com>
2021-11-16 23:19:50 +0900

    メッセージ応答機能text以外の対応追加修正

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -17,40 +17,39 @@ class LineMessengerController extends Controller
     public function webhook(Request $request) {
         // LINEから送られた内容を$inputsに代入
         $inputs=$request->all();
-
-        $reply_token = $inputs['events'][0]['replyToken'];
-        // そこからtypeをとりだし、$message_typeに代入
-        $hook_type=$inputs['events'][0]['message']['type'];
-
-        // LINEBOTSDKの設定
-        $http_client = new CurlHTTPClient(config('services.line.channel_token'));
-        $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
-        $reply_message=new TextMessageBuilder('ご返信ありがとうございます。');
+        $hook_type=$inputs['events'][0]['type'];
 
         // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
         if($hook_type=='message') {
-            // LINEのユーザーIDをuserIdに代入
-            $clientId=$inputs['events'][0]['source']['userId'];
-            $client=Client::where('line_id', $clientId)->first();
-            // userIdがあるユーザーを検索
-            // もし見つからない場合は、データベースに保存
-            if($client==NULL) {
-                $client=new Client();
-                $client->line_id=$clientId;
-                $client->name=$inputs['events'][0]['message']['text'];
-                $client->save();
-                // 送信するメッセージの設定
-                $reply_message=new TextMessageBuilder('ご返信ありがとうございます。登録が完了しました！');
+            $reply_token = $inputs['events'][0]['replyToken'];
+            $message_type=$inputs['events'][0]['message']['type'];
+            // LINEBOTSDKの設定
+            $http_client = new CurlHTTPClient(config('services.line.channel_token'));
+            $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
+            $reply_message=new TextMessageBuilder('ご返信ありがとうございます。');
+            if($message_type=='text'){
+                // LINEのユーザーIDをuserIdに代入
+                $clientId=$inputs['events'][0]['source']['userId'];
+                $client=Client::where('line_id', $clientId)->first();
+                // userIdがあるユーザーを検索
+                // もし見つからない場合は、データベースに保存
+                if($client==NULL) {
+                    $client=new Client();
+                    $client->line_id=$clientId;
+                    $client->name=$inputs['events'][0]['message']['text'];
+                    $client->save();
+                    // 送信するメッセージの設定
+                    $reply_message=new TextMessageBuilder('ご返信ありがとうございます。登録が完了しました！');
+                }else{
+                    // 送信するメッセージの設定
+                    $reply_message=new TextMessageBuilder('ご返信ありがとうございます。すでにご登録いただいております');
+                }
             }else{
-                // 送信するメッセージの設定
-                $reply_message=new TextMessageBuilder('ご返信ありがとうございます。すでにご登録いただいております');
+                $reply_message=new TextMessageBuilder('ご返信ありがとうございます。申し訳ございませんが、文章を用いてお名前をご返信ください');
             }
-        }else{
-            // 送信するメッセージの設定
-            $reply_message=new TextMessageBuilder('ご返信ありがとうございます。申し訳ございませんが、文章を用いてお名前をご返信ください');
+            $reply=$bot->replyMessage($reply_token, $reply_message);
+            return 'ok';
         }
-        $reply=$bot->replyMessage($reply_token, $reply_message);
-        return 'ok';
     }
 
     public function message() {
