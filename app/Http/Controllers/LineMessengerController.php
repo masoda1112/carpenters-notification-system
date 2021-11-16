eb7fe170736b8d8d92302cdc45d3983505b26677
masoda1112 <wendy.odakura@gmail.com>
2021-11-16 19:34:36 +0900

    メッセージ応答機能実装中

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -15,9 +15,16 @@ class LineMessengerController extends Controller
     public function webhook(Request $request) {
         // LINEから送られた内容を$inputsに代入
         $inputs=$request->all();
+
+        $reply_token = $inputs['events'][0]['replyToken'];
         // そこからtypeをとりだし、$message_typeに代入
         $hook_type=$inputs['events'][0]['type'];
 
+        // LINEBOTSDKの設定
+        $http_client = new CurlHTTPClient(config('services.line.channel_token'));
+        $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
+
+        $reply_message=new TextMessageBuilder('ご返信ありがとうございます。');
 
         // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
         if($hook_type=='message') {
@@ -27,28 +34,23 @@ class LineMessengerController extends Controller
             // userIdがあるユーザーを検索
             // もし見つからない場合は、データベースに保存
             if($client==NULL) {
-                // 送信するメッセージの設定
-                // LINEBOTSDKの設定
-                $reply_token=$inputs['events'][0]['replyToken'];
-                $http_client=new CurlHTTPClient(config('services.line.channel_token'));
-                $bot=new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
-                $reply_message=new TextMessageBuilder('ご返信ありがとうございます。登録が完了しました！');
-                $reply=$bot->replyText($reply_token, $reply_message);
                 $client=new Client();
                 $client->line_id=$clientId;
                 $client->name=$inputs['events'][0]['message']['text'];
                 $client->save();
+
+                // 送信するメッセージの設定
+                $reply_message=new TextMessageBuilder('ご返信ありがとうございます。登録が完了しました！');
             }else{
                 // 送信するメッセージの設定
                 $reply_message=new TextMessageBuilder('ご返信ありがとうございます。すでに登録されております。');
-                $reply=$bot->replyText($reply_token, $reply_message);
             }
             return 'ok';
         }else{
             // 送信するメッセージの設定
             $reply_message=new TextMessageBuilder('ご返信ありがとうございます。申し訳ございませんが、文章を用いてお名前をご返信ください');
-            $reply=$bot->replyText($reply_token, $reply_message);
         }
+        $reply=$bot->replyMessage($reply_token, $reply_message);
     }
 
     public function message() {
