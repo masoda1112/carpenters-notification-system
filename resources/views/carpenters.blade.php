30fbf6ef120c632d573b0f33da8118406b701c50
masoda1112 <wendy.odakura@gmail.com>
2021-11-16 16:47:16 +0900

    lineapiwebhookテスト修正

--- resources/views/carpenters.blade.php
+++ resources/views/carpenters.blade.php
@@ -5,7 +5,7 @@
         <a class="create-page-link" href="/carpenters/new">職人を追加する</a>
     </div>
     <div class="index-area">
-        <h1>職人一覧{{config('services.line.channel_token')}}</h1>
+        <h1>職人一覧{{config('services.line.channel_token')}}{{env('POSTMARK_TOKEN')}}{{env('LINE_MESSENGER_SECRET')}}</h1>
         <div class="index-contents">
             <table class="index-table">
                 <thead>
