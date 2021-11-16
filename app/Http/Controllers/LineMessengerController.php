5fdff67c0b69288f6a19e34d594493fcd2d575ee
masoda1112 <wendy.odakura@gmail.com>
2021-11-16 18:05:28 +0900

    メッセージ送信機能実装

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -54,4 +54,22 @@ class LineMessengerController extends Controller
         }
         $reply=$bot->replyText($reply_token, $reply_message);
     }
+
+    public function message() {
+
+        // LINEBOTSDKの設定
+        $http_client = new CurlHTTPClient(config('services.line.channel_token'));
+        $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
+
+        // LINEユーザーID指定
+        $userId = "Uc2c045417716ec2c862eff037622e2d9";
+
+        // メッセージ設定
+        $message = "こんにちは！";
+
+        // メッセージ送信
+        $textMessageBuilder = new TextMessageBuilder($message);
+        $response = $bot->pushMessage($userId, $textMessageBuilder);
+
+    }
 }
