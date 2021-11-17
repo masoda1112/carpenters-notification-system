7b8e354828c153f293fd40a0124aeaee6c433260
masoda1112 <wendy.odakura@gmail.com>
2021-11-17 10:14:25 +0900

    プッシュメッセージ機能デバッグ

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -62,9 +62,9 @@ class LineMessengerController extends Controller
         $today = date("Y-m-d");
         $messages = Message::where('date', $today)->get();
         foreach($messages as $message){
-            $userId = $message->cliend_id;
-            $lineMessage = new TextMessageBuilder($message->message);
-            $response = $bot->pushMessage($userId, $textMessageBuilder);
+            // $userId = $message->cliend_id;
+            // $lineMessage = new TextMessageBuilder($message->message);
+            // $response = $bot->pushMessage($userId, $textMessageBuilder);
             // foreach($message->carpenters as $carpenter){
             //     $lineImgMessage = new ImageMessageBuilder($carpenter->img,$carpenter->img);
             //     $response = $bot->pushMessage($userId, $lineImgMessage);
