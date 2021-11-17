218fb7784063a48fc8a9009dfa3497956f794269
masoda1112 <wendy.odakura@gmail.com>
2021-11-17 09:29:07 +0900

    プッシュメッセージ機能デバッグ

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -16,38 +16,38 @@ class LineMessengerController extends Controller
     //
     public function webhook(Request $request) {
         // LINEから送られた内容を$inputsに代入
-        $inputs=$request->all();
-        $hook_type=$inputs['events'][0]['type'];
+        $inputs = $request->all();
+        $hook_type = $inputs['events'][0]['type'];
 
         // メッセージが送られた場合、$message_typeは'message'となる。その場合処理実行。
-        if($hook_type=='message') {
+        if($hook_type == 'message') {
             $reply_token = $inputs['events'][0]['replyToken'];
-            $message_type=$inputs['events'][0]['message']['type'];
+            $message_type = $inputs['events'][0]['message']['type'];
             // LINEBOTSDKの設定
             $http_client = new CurlHTTPClient(config('services.line.channel_token'));
             $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
-            $reply_message=new TextMessageBuilder('ご返信ありがとうございます。');
-            if($message_type=='text'){
+            $reply_message = new TextMessageBuilder('ご返信ありがとうございます。');
+            if($message_type == 'text'){
                 // LINEのユーザーIDをuserIdに代入
-                $clientId=$inputs['events'][0]['source']['userId'];
-                $client=Client::where('line_id', $clientId)->first();
+                $clientId = $inputs['events'][0]['source']['userId'];
+                $client = Client::where('line_id', $clientId)->first();
                 // userIdがあるユーザーを検索
                 // もし見つからない場合は、データベースに保存
-                if($client==NULL) {
-                    $client=new Client();
-                    $client->line_id=$clientId;
-                    $client->name=$inputs['events'][0]['message']['text'];
+                if($client == NULL) {
+                    $client = new Client();
+                    $client->line_id = $clientId;
+                    $client->name = $inputs['events'][0]['message']['text'];
                     $client->save();
                     // 送信するメッセージの設定
-                    $reply_message=new TextMessageBuilder('ご返信ありがとうございます。登録が完了しました！');
+                    $reply_message = new TextMessageBuilder('ご返信ありがとうございます。登録が完了しました！');
                 }else{
                     // 送信するメッセージの設定
-                    $reply_message=new TextMessageBuilder('ご返信ありがとうございます。すでにご登録いただいております');
+                    $reply_message = new TextMessageBuilder('ご返信ありがとうございます。すでにご登録いただいております');
                 }
             }else{
-                $reply_message=new TextMessageBuilder('ご返信ありがとうございます。申し訳ございませんが、文章を用いてお名前をご返信ください');
+                $reply_message = new TextMessageBuilder('ご返信ありがとうございます。申し訳ございませんが、文章を用いてお名前をご返信ください');
             }
-            $reply=$bot->replyMessage($reply_token, $reply_message);
+            $reply = $bot->replyMessage($reply_token, $reply_message);
             return 'ok';
         }
     }
@@ -60,7 +60,7 @@ class LineMessengerController extends Controller
 
         // 日付が今日のメッセージを取得
         $today = date("Y-m-d");
-        $messages = Message::where('date',$today)->get();
+        $messages = Message::where('date',$today)->first();
         var_dump($messages);
         foreach($messages as $message){
             $userId = $message->cliend_id;
