ab32525ab7c17515b7a45914de6a76dec96d82ee
masoda1112 <wendy.odakura@gmail.com>
2021-11-17 09:39:01 +0900

    プッシュメッセージ機能デバッグ

--- app/Http/Controllers/LineMessengerController.php
+++ app/Http/Controllers/LineMessengerController.php
@@ -61,7 +61,7 @@ class LineMessengerController extends Controller
 
         // 日付が今日のメッセージを取得
         $today = date("Y-m-d");
-        $messages = DB::table()->where('date',$today);
+        $messages = DB::table()->where('date',$today)->first();
         var_dump($messages);
         // foreach($messages as $message){
         //     $userId = $message->cliend_id;
