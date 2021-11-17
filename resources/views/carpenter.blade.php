67417a9feee8600251d5831448be65ecde45ea49
masoda1112 <wendy.odakura@gmail.com>
2021-11-17 16:18:03 +0900

    デザイン修正

--- resources/views/carpenter.blade.php
+++ resources/views/carpenter.blade.php
@@ -29,7 +29,7 @@
                 </div>
                 <div class="input-item">
                     <p class="input-item-title">画像</p>
-                    <div class="d-flex flex-column">
+                    <div class="d-flex flex-column over-flow-hidden">
                         <label class="d-block over-flow-hidden" for="img">現在の使用している画像：{{$carpenter->img}}</label>
                         <input class="no-border input-item-file" type="file" name="img" accept="image/jpeg, image/png">
                     </div>
