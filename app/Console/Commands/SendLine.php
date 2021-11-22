950b6499165e0e1b3226b85d52b0ae39c65a6253
masoda1112 <wendy.odakura@gmail.com>
2021-11-22 18:29:34 +0900

    バッチ処理デバッグ

--- app/Console/Commands/SendLine.php
+++ app/Console/Commands/SendLine.php
@@ -4,9 +4,11 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log;
+use App\Models\Client;
 use App\Models\Message;
 use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 use LINE\LINEBot;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
 use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 class SendLine extends Command
@@ -42,26 +44,26 @@ class SendLine extends Command
      */
     public function handle()
     {
-        // $message = '[' . date('Y-m-d h:i:s') . ']MessageCount:' . Message::count();
-        // $this->info( $message );
-        // Log::setDefaultDriver('batch');
-        // Log::info( $message );
-        // return Command::SUCCESS;
-
         // LINEBOTSDKの設定
         $http_client = new CurlHTTPClient(config('services.line.channel_token'));
         $bot = new LINEBot($http_client, ['channelSecret' => config('services.line.messenger_secret')]);
 
-        // LINEユーザーID指定
-        $userId = "1656616138";
-
-        // メッセージ設定
-        $message = "こんにちは！";
-
-        // メッセージ送信
-        $textMessageBuilder = new TextMessageBuilder($message);
-        $response = $bot->pushMessage($userId, $textMessageBuilder);
-        $this->info($response->getHTTPStatus());
-        $this->info($response->getRawBody());
+        // 日付が今日のメッセージを取得
+        $today = date("Y-m-d");
+        $messages = Message::where('date', $today)->get();
+        foreach($messages as $message){
+            $userId = $message->client->line_id;
+            $this->info( $userId );
+            // $lineMessage = new TextMessageBuilder($message->message);
+            // $response = $bot->pushMessage($userId, $lineMessage);
+            // foreach($message->carpenters as $carpenter){
+            //     $lineImgMessage = new ImageMessageBuilder(secure_url($carpenter->img),secure_url($carpenter->img));
+            //     $lineCarpenterMessage = new TextMessageBuilder($carpenter->profile);
+            //     $response = $bot->pushMessage($userId, $lineCarpenterMessage);
+            //     $response = $bot->pushMessage($userId, $lineImgMessage);
+            // }
+            // $message->status = true;
+            // $message->save();
+        }
     }
 }
