c9259ea5f76e2733b09314a34e6a8d8cf8720ec1
masoda1112 <wendy.odakura@gmail.com>
2021-11-22 15:51:27 +0900

    画像表示のためにcloudinary導入中デバッグ

--- app/Http/Controllers/CarpenterController.php
+++ app/Http/Controllers/CarpenterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Carpenter;
 use App\Http\Requests\CarpenterRequest;
+use JD\Cloudder\Facades\Cloudder;
 
 class CarpenterController extends Controller
 {
@@ -27,19 +28,19 @@ class CarpenterController extends Controller
         $carpenter = new Carpenter();
         $carpenter->name = $request->name;
         $carpenter->profile = $request->profile;
-        $image_path = $image->getRealPath();
+        $image_path = $request->img->getRealPath();
         var_dump($image_path);
-        // Cloudder::upload($image_path, null);
-        // $logoUrl = Cloudder::secureShow($publicId, [
-        //     'width'     => 200,
-        //     'height'    => 200
-        // ]);
-        // $carpenter->img = $logoUrl;
+        Cloudder::upload($image_path, null);
+        $logoUrl = Cloudder::secureShow($publicId, [
+            'width'     => 200,
+            'height'    => 200
+        ]);
+        $carpenter->img = $logoUrl;
 
-        // // $carpenter->img = base64_encode(file_get_contents($request->img->getRealPath()));
-        // $carpenter->role = $request->role;
-        // $carpenter->save();
-        // return redirect('/carpenters');
+        // $carpenter->img = base64_encode(file_get_contents($request->img->getRealPath()));
+        $carpenter->role = $request->role;
+        $carpenter->save();
+        return redirect('/carpenters');
     }
 
     public function update(CarpenterRequest $request, Carpenter $carpenter){
