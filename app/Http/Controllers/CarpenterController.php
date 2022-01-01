8c8300ff53b9778a54ca0622259e84348300c8c8
masoda1112 <wendy.odakura@gmail.com>
2022-01-01 17:33:59 +0900

    大工の追加機能エラー検証中

--- app/Http/Controllers/CarpenterController.php
+++ app/Http/Controllers/CarpenterController.php
@@ -26,23 +26,24 @@ class CarpenterController extends Controller
 
     public function create(CarpenterRequest $request){
         $carpenter = new Carpenter();
-        var_dump($request->name);
-        var_dump($request->profile);
-        var_dump($request->role);
-        var_dump($request->img->getRealPath());
-        // $carpenter->name = $request->name;
-        // $carpenter->profile = $request->profile;
-        // $image_path = $request->img->getRealPath();
-        // Cloudder::upload($image_path, null);
-        // $publicId = Cloudder::getPublicId();
-        // $logoUrl = Cloudder::secureShow($publicId, [
-        //     'width'     => 500,
-        //     'height'    => 500
-        // ]);
-        // $carpenter->img = $logoUrl;
-        // $carpenter->cloudinary_public_id = $publicId;
-        // // $carpenter->img = base64_encode(file_get_contents($request->img->getRealPath()));
-        // $carpenter->role = $request->role;
+
+        $carpenter->name = $request->name;
+        $carpenter->profile = $request->profile;
+        $image_path = $request->img->getRealPath();
+        Cloudder::upload($image_path, null);
+        $publicId = Cloudder::getPublicId();
+        $logoUrl = Cloudder::secureShow($publicId, [
+            'width'     => 500,
+            'height'    => 500
+        ]);
+        $carpenter->img = $logoUrl;
+        $carpenter->cloudinary_public_id = $publicId;
+        // $carpenter->img = base64_encode(file_get_contents($request->img->getRealPath()));
+        $carpenter->role = $request->role;
+        var_dump($carpenter->name);
+        var_dump($carpenter->profile);
+        var_dump($carpenter->role);
+        var_dump($carpenter->img->getRealPath());
         // $carpenter->save();
         // return redirect('/carpenters');
     }
