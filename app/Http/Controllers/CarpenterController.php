de19fc104b7a06873490d7796309ae5df1c1483d
masoda1112 <wendy.odakura@gmail.com>
2022-01-02 22:51:04 +0900

    大工の追加機能エラー検証中

--- app/Http/Controllers/CarpenterController.php
+++ app/Http/Controllers/CarpenterController.php
@@ -36,11 +36,11 @@ class CarpenterController extends Controller
         //     'width'     => 500,
         //     'height'    => 500
         // ]);
-        $carpenter->img = $logoUrl;
-        $carpenter->cloudinary_public_id = $publicId;
-        // $carpenter->img = base64_encode(file_get_contents($request->img->getRealPath()));
-        $carpenter->role = $request->role;
-        var_dump($carpenter->name,$carpenter->profile,$carpenter->role, $image_path,$publicId);
+        // $carpenter->img = $logoUrl;
+        // $carpenter->cloudinary_public_id = $publicId;
+        // // $carpenter->img = base64_encode(file_get_contents($request->img->getRealPath()));
+        // $carpenter->role = $request->role;
+        // var_dump($carpenter->name,$carpenter->profile,$carpenter->role, $image_path,$publicId);
         // $carpenter->save();
         // return redirect('/carpenters');
     }
