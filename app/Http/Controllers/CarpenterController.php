4e2fafc6392d8d396da3a90e0db8905aeca011fd
masoda1112 <wendy.odakura@gmail.com>
2022-01-05 12:54:30 +0900

    cloudinary->awss3 デバッグ

--- app/Http/Controllers/CarpenterController.php
+++ app/Http/Controllers/CarpenterController.php
@@ -34,7 +34,7 @@ class CarpenterController extends Controller
         // $file = $params['img'];
         // ↓で500エラーが起こる
         // var_dump($image_path);
-        var_dump($image);
+        // var_dump($image);
         $path = Storage::disk('s3')->putFile('carpenters-notification-system', $image, 'public');
         var_dump($path);
         // Cloudder::upload($image_path, null);
