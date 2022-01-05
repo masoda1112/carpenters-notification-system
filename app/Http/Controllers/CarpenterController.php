2d840ad78149fcf04b20065fc3366058a8928d89
masoda1112 <wendy.odakura@gmail.com>
2022-01-05 12:53:07 +0900

    cloudinary->awss3 デバッグ

--- app/Http/Controllers/CarpenterController.php
+++ app/Http/Controllers/CarpenterController.php
@@ -43,12 +43,12 @@ class CarpenterController extends Controller
         //     'width'     => 500,
         //     'height'    => 500
         // ]);
-        // $carpenter->img = $path;
-        // $carpenter->cloudinary_public_id = $publicId;
-        // // $carpenter->img = base64_encode(file_get_contents($request->img->getRealPath()));
-        // $carpenter->role = $request->role;
-        // $carpenter->save();
-        // return redirect('/carpenters');
+        $carpenter->img = $path;
+        $carpenter->cloudinary_public_id = $path;
+        // $carpenter->img = base64_encode(file_get_contents($request->img->getRealPath()));
+        $carpenter->role = $request->role;
+        $carpenter->save();
+        return redirect('/carpenters');
     }
 
     public function update(CarpenterRequest $request, Carpenter $carpenter){
