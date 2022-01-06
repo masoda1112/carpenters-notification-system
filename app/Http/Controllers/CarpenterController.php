b681ded75bbd6a05dda5a0d16ee0ef13b4ddfb0a
masoda1112 <wendy.odakura@gmail.com>
2022-01-06 17:42:53 +0900

    デバッグモードなるものを試す

--- app/Http/Controllers/CarpenterController.php
+++ app/Http/Controllers/CarpenterController.php
@@ -54,14 +54,15 @@ class CarpenterController extends Controller
     private function postImage(CarpenterRequest $request,Carpenter $carpenter) :void
     {
         $image_path = $request->img->getRealPath();
-        Cloudder::upload($image_path, null);
-        $publicId = Cloudder::getPublicId();
-        // $logoUrl = Cloudder::secureShow($publicId, [
-        //     'width'     => 500,
-        //     'height'    => 500
-        // ]);
-        $logoUrl = Cloudder::secureShow($publicId);
-        $carpenter->img = $logoUrl;
-        $carpenter->cloudinary_public_id = $publicId;
+        var_dump($image_path);
+        // Cloudder::upload($image_path, null);
+        // $publicId = Cloudder::getPublicId();
+        // // $logoUrl = Cloudder::secureShow($publicId, [
+        // //     'width'     => 500,
+        // //     'height'    => 500
+        // // ]);
+        // $logoUrl = Cloudder::secureShow($publicId);
+        // $carpenter->img = $logoUrl;
+        // $carpenter->cloudinary_public_id = $publicId;
     }
 }
